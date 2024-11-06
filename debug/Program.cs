f0393874b5006afbaba44232985f9caea31e82b5
sky line <32512284+linesky@users.noreply.github.com>
2024-11-06 17:54:27 +0000

    Add files via upload

---
+++ debug/Program.cs
@@ -0,0 +1,10 @@
+﻿void ddebugs(string a){
+    string s="";
+    Console.WriteLine(a+"\nwait a key enter.");
+    s=Console.ReadLine()+"";
+    s=s+"";
+}
+
+int i=0;
+for(i=0;i<10;i++)ddebugs(i.ToString());
+
