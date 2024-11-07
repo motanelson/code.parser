5596a7885ab218f97d3ecc9bad97b5aa3c68ec9b
sky line <32512284+linesky@users.noreply.github.com>
2024-11-07 14:47:23 +0000

    Add files via upload

--- add/Program.cs
+++ add/Program.cs
@@ -1,4 +1,14 @@
-﻿int adds(int a,int b){
-    return a+b;
+﻿using System;
+
+class MainApp
+{
+    static int adds(int a,int b){
+        return a+b;
+    }
+    
+
+    public static void Main()
+    {
+        Console.WriteLine("{0}",adds(10,10));
+    }
 }
-Console.WriteLine("{0}",adds(10,10));
