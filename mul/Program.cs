89d1f8587cbf1ecb4142c4f52849a569e7c16542
sky line <32512284+linesky@users.noreply.github.com>
2024-11-07 15:12:51 +0000

    Add files via upload

--- mul/Program.cs
+++ mul/Program.cs
@@ -2,14 +2,13 @@
 
 class MainApp
 {
-    static int divs(int a,int b){
-        return a/b;
+    static int mults(int a,int b){
+        return a*b;
     }
 
     public static void Main()
     {
-        Console.WriteLine("{0}",divs(20,10));
+        Console.WriteLine("{0}",mults(20,10));
     }
 }
-
 
