70b87a623a945fa94d7fa6f47fc512f74ecf20e3
sky line <32512284+linesky@users.noreply.github.com>
2024-11-07 15:09:55 +0000

    Add files via upload

--- div/Program.cs
+++ div/Program.cs
@@ -1,4 +1,15 @@
-﻿int divs(int a,int b){
-    return a/b;
+﻿using System;
+
+class MainApp
+{
+    static int divs(int a,int b){
+        return a/b;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("{0}",divs(20,10));
+    }
 }
-Console.WriteLine("{0}",divs(20,10));
+
+
