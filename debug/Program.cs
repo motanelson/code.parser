7e49e2d0d1512d02291762a64e0d34a2961332f2
sky line <32512284+linesky@users.noreply.github.com>
2024-11-07 15:30:50 +0000

    Add files via upload

--- debug/Program.cs
+++ debug/Program.cs
@@ -1,10 +1,21 @@
-﻿void ddebugs(string a){
-    string s="";
-    Console.WriteLine(a+"\nwait a key enter.");
-    s=Console.ReadLine()+"";
-    s=s+"";
+﻿using System;
+
+class MainApp
+{
+    static void ddebugs(string a){
+        string s="";
+        Console.WriteLine(a+"\nwait a key enter.");
+        s=Console.ReadLine()+"";
+        s=s+"";
+     }
+
+    public static void Main()
+    {
+        int i=0;
+        for(i=0;i<10;i++)ddebugs(i.ToString());
+
+    }
 }
 
-int i=0;
-for(i=0;i<10;i++)ddebugs(i.ToString());
+
 
